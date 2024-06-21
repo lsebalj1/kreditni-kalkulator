daccd5d3cfa19978251db47508eef0727d416b23
Gabrijel Mirko <130052353+gmirko1@users.noreply.github.com>
2024-06-22 01:15:54 +0200

    tr

--- kredit.py
+++ kredit.py
@@ -1,11 +1,16 @@
+
 from baza import db
 
 class Kredit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    banka_id = db.Column(db.Integer, db.ForeignKey('banka.id'), nullable=False)
-    vrsta_kredita = db.Column(db.String(80), nullable=False)
+    vrsta = db.Column(db.String(80), nullable=False)
     kamatna_stopa = db.Column(db.Float, nullable=False)
     iznos = db.Column(db.Float, nullable=False)
+    datum_pocetka = db.Column(db.Date, nullable=False)
+    trajanje = db.Column(db.Integer, nullable=False)
+    mjesecna_rata = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    duznik = db.Column(db.String(100), nullable=False)
 
-    def izracunaj_trosak(self):
-        return self.iznos + (self.iznos * (self.kamatna_stopa / 100))
+    def repr(self):
+        return f'<Kredit {self.vrsta}>'
