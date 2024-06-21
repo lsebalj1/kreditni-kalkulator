fca47040987134d1dcb78a41e8257eaa29b741b2
Gabrijel Mirko <130052353+gmirko1@users.noreply.github.com>
2024-06-22 01:13:32 +0200

    df

--- banka.py
+++ banka.py
@@ -4,5 +4,11 @@ class Banka(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     naziv = db.Column(db.String(80), nullable=False)
     adresa = db.Column(db.String(200), nullable=False)
-    utjecaj_na_stopu = db.Column(db.Float, nullable=False) 
+    broj_telefona = db.Column(db.String(20), nullable=True)  
+    utjecaj_na_stopu = db.Column(db.Float, nullable=False)
+    kredit_id = db.Column(db.Integer, db.ForeignKey('kredit.id'), nullable=True)  
     krediti = db.relationship('Kredit', backref='banka', lazy=True)
+
+    def repr(self):
+        return f'<Banka {self.naziv}>'
+    
